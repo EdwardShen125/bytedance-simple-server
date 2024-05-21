844d4407ab5815fcf7cd60affc98fbeb41763de3
EdwardShen125 <mg495430312@gmail.com>
2024-05-21 11:40:35 +0800

    💍test(test): test for utils

--- pkg/oss/utils_test.go
+++ pkg/oss/utils_test.go
@@ -93,3 +93,29 @@ func TestPublishCoverToOss(t *testing.T) {
 		})
 	}
 }
+
+func TestQueryOssCoverURL(t *testing.T) {
+	type args struct {
+		objectKey string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := QueryOssCoverURL(tt.args.objectKey)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QueryOssCoverURL() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("QueryOssCoverURL() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
