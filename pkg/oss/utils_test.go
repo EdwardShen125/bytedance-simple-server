156b4ebb4963cd962596c6b3063b29f397570f67
EdwardShen125 <mg495430312@gmail.com>
2024-05-21 11:39:48 +0800

    💍test(test): for utils

--- pkg/oss/utils_test.go
+++ pkg/oss/utils_test.go
@@ -1,6 +1,9 @@
 package oss
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestPublishVideoToPublic(t *testing.T) {
 	type args struct {
@@ -65,6 +68,27 @@ func TestQueryOssVideoURL(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("QueryOssVideoURL() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPublishCoverToOss(t *testing.T) {
+	type args struct {
+		objectKey   string
+		coverReader *bytes.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := PublishCoverToOss(tt.args.objectKey, tt.args.coverReader); (err != nil) != tt.wantErr {
+				t.Errorf("PublishCoverToOss() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
