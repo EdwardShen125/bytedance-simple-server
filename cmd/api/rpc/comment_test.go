1f4708d707e95cace0af0574fc02e0e261f964a7
EdwardShen125 <mg495430312@gmail.com>
2024-06-03 18:37:30 +0800

    💍test(test): for comment action

--- cmd/api/rpc/comment_test.go
+++ cmd/api/rpc/comment_test.go
@@ -33,3 +33,30 @@ func TestCommentList(t *testing.T) {
 		})
 	}
 }
+
+func TestCommentAction(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req *comment.CommentActionRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *comment.Comment
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CommentAction(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CommentAction() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CommentAction() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
